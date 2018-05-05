b8b21d8aa782f3e3da48470063d20e7b75f6a33c
brianberzins <brianberzins@gmail.com>
2018-05-05 14:35:50 -0500

    0.16.41 seablock

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ docker run \
   --volume /home/murasaki/.factorio/server-settings:/factorio/server-settings \
   factorio:latest \
   --server-settings /factorio/server-settings/server-settings.json \
-  --start-server /factorio/saves/bobs-mods.zip \
+  --start-server /factorio/saves/marathon-seablock.zip \
   --mod-directory /factorio/mods
